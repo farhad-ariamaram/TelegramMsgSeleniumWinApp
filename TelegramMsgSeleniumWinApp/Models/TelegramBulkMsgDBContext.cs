b08e99a1ac4acc1440ef2c419ecec1d7342e5c56
ym <ym@DESKTOP-2HUH2VU>
2021-07-12 11:26:27 +0430

    complete

---
+++ TelegramMsgSeleniumWinApp/Models/TelegramBulkMsgDBContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace TelegramMsgSeleniumWinApp.Models
+{
+    public partial class TelegramBulkMsgDBContext : DbContext
+    {
+        public TelegramBulkMsgDBContext()
+        {
+        }
+
+        public TelegramBulkMsgDBContext(DbContextOptions<TelegramBulkMsgDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.;Database=TelegramBulkMsgDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.Property(e => e.LastSeen).HasMaxLength(255);
+
+                entity.Property(e => e.Phone).HasMaxLength(255);
+
+                entity.Property(e => e.SendDate).HasColumnType("datetime");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
--- TelegramMsgSeleniumWinApp/Program.cs
+++ TelegramMsgSeleniumWinApp/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using TelegramMsgSeleniumWinApp.Models;
 
 namespace TelegramMsgSeleniumWinApp
 {
@@ -11,8 +12,49 @@ namespace TelegramMsgSeleniumWinApp
     {
         static async Task Main(string[] args)
         {
+            TelegramBulkMsgDBContext _db = new TelegramBulkMsgDBContext();
+
             ChromeOptions options = new ChromeOptions();
             options.AddArgument(@"user-data-dir=C:\Users\ym\AppData\Local\Google\Chrome\User Data\Default");
+
+            string lastseen = "";
+
+            var msg =
+                "مشتریان عزیز" +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                "درود بر شما " +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                "با احترام ضمن آرزوی سلامتی و بهروزی " +
+                "بدینوسیله به اطلاع میرساند که شرکت پرنده باران پارس" +
+                "با نام تجاری ABAone" +
+                "فروشگاه اینترنتی خود را در لینک زیر فعال نموده است." +
+                "موفق و پیروز باشید" +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                "انصاری" +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                "تلفن واحد فروش :" +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                OpenQA.Selenium.Keys.Shift +
+                "02191013200" +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                "موبایل واحد فروش :" +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                "09199122519" +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                "https://abaone.ir/shop/" +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                "اینستاگرام:" +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                OpenQA.Selenium.Keys.Shift + OpenQA.Selenium.Keys.Enter +
+                "instagram: https://www.instagram.com/abaone.ir";
 
             IWebDriver driver = new ChromeDriver(options);
 
@@ -66,6 +108,40 @@ namespace TelegramMsgSeleniumWinApp
 
                     if (found)
                     {
+                        await Task.Delay(2000);
+
+                        driver.FindElement(By.XPath(@"/html/body/div[1]/div[2]/div/div[2]/div[3]/div/div[3]/div[2]/div/div/div/form/div[2]/div[5]")).SendKeys(msg);
+
+                        await Task.Delay(2000);
+
+                        driver.FindElement(By.XPath(@"/html/body/div[1]/div[2]/div/div[2]/div[3]/div/div[3]/div[2]/div/div/div/form/div[3]/button/span[1]")).Click();
+
+                        await Task.Delay(2000);
+
+                        driver.FindElement(By.XPath(@"/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/a/div/span[1]")).Click();
+
+                        await Task.Delay(2000);
+
+                        driver.FindElement(By.XPath(@"/html/body/div[6]/div[2]/div/div/div[3]/div/div[3]/div[2]/a")).Click();
+
+                        await Task.Delay(2000);
+
+                        driver.FindElement(By.XPath(@"/html/body/div[6]/div[2]/div/div/div[3]/div/div[3]/div[2]/a")).Click();
+
+                        await Task.Delay(2000);
+
+                        driver.FindElement(By.XPath(@"/html/body/div[6]/div[2]/div/div/div[1]/div[1]/div[1]/a")).Click();
+
+                        await Task.Delay(2000);
+
+                        try
+                        {
+                            lastseen = driver.FindElement(By.XPath(@"/html/body/div[1]/div[1]/div/div/div[2]/div/div[2]/a/div/span[2]/span")).Text;
+                        }
+                        catch (Exception)
+                        {
+                            lastseen = "";
+                        }
 
                     }
                     else
@@ -79,10 +155,21 @@ namespace TelegramMsgSeleniumWinApp
                         driver.FindElement(By.XPath(@"/html/body/div[6]/div[2]/div/div/div[1]/div[1]/div/a[1]")).Click();
 
                     }
-                } 
-            }
 
+                    Person person = new Person
+                    {
+                        Phone = line,
+                        SendDate = DateTime.Now,
+                        HasTelegram = lastseen == "" ? false : true,
+                        LastSeen = lastseen
+                    };
+
+                    await _db.Persons.AddAsync(person);
+                    await _db.SaveChangesAsync();
+            }
         }
 
     }
+
+}
 }
